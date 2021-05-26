7228a6463718cdd4750a43c26c9616cba54ced86
Bianca Brown <biancakbrown58@gmail.com>
2021-05-26 13:07:40 -0400

    got one slider to move

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import React, { Component } from 'react'
 
 export class App extends Component {
+  state = {
+    hue: 0,
+  }
+  handleChange = event => {
+    this.setState({ hue: event.target.value })
+  }
+
   render() {
-    return <div>Hello, World!</div>
+    return (
+      <div>
+        <div className="color-bar">-</div>
+        <input
+          type="range"
+          min="0"
+          max="255"
+          className="hue"
+          value={this.state.hue}
+          onChange={this.handleChange}
+        ></input>
+      </div>
+    )
   }
 }
