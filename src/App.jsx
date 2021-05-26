3b03ad72a6e5aa45376bc86b09bdc03109ec8e6e
Bianca Brown <biancakbrown58@gmail.com>
2021-05-26 14:53:17 -0400

    adding alpha

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,77 @@ import React, { Component } from 'react'
 
 export class App extends Component {
   state = {
-    hue: 0,
+    hue: Math.floor(Math.random() * (359 - 1) + 1),
+    saturation: Math.floor(Math.random() * (100 - 1) + 1),
+    lightness: Math.floor(Math.random() * (100 - 1) + 1),
+    alpha: Math.floor(Math.random() * (100 - 1) + 1),
   }
-  handleChange = event => {
-    this.setState({ hue: event.target.value })
+
+  handleButtonClick = event => {
+    const newHue = Math.floor(Math.random() * (359 - 1) + 1)
+    const newSaturation = Math.floor(Math.random() * (100 - 1) + 1)
+    const newLightness = Math.floor(Math.random() * (100 - 1) + 1)
+    const newAlpha = Math.floor(Math.random() * (100 - 1) + 1)
+
+    const newState = {
+      hue: newHue,
+      saturation: newSaturation,
+      lightness: newLightness,
+      alpha: newAlpha,
+    }
+    this.setState(newState)
   }
 
   render() {
+    const newBackgroundColor = `hsl(${this.state.hue},${this.state.saturation}%,${this.state.lightness}%,${this.state.alpha}%)`
+
+    const newStyle = { backgroundColor: newBackgroundColor }
     return (
       <div>
-        <div className="color-bar">-</div>
+        <div className="color-bar" style={newStyle}>
+          {`hsla(${this.state.hue},${this.state.saturation}%,${this.state.lightness}%,${this.state.alpha}%)`}
+        </div>
+        <button style={newStyle} onClick={this.handleButtonClick}>
+          Get a Random Color
+        </button>
+        <p>Hue: {this.state.hue}</p>
         <input
           type="range"
           min="0"
-          max="255"
-          className="hue"
+          max="359"
+          className="hue slider"
           value={this.state.hue}
-          onChange={this.handleChange}
+          onChange={event => this.setState({ hue: event.target.value })}
+        ></input>
+
+        <p>Saturation: {this.state.saturation}</p>
+        <input
+          type="range"
+          min="0"
+          max="100"
+          className="saturation slider"
+          value={this.state.saturation}
+          onChange={event => this.setState({ saturation: event.target.value })}
+        ></input>
+
+        <p>Lightness: {this.state.lightness}</p>
+        <input
+          type="range"
+          min="0"
+          max="100"
+          className="lightness slider"
+          value={this.state.lightness}
+          onChange={event => this.setState({ lightness: event.target.value })}
+        ></input>
+
+        <p>Alpha: {this.state.alpha}</p>
+        <input
+          type="range"
+          min="0"
+          max="100"
+          className="alpha slider"
+          value={this.state.alpha}
+          onChange={event => this.setState({ alpha: event.target.value })}
         ></input>
       </div>
     )
